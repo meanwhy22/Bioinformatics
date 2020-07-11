ec083187f110b4bb628afe641713203196497c09
Dr Sanjay Rathee <sanjaysinghrathi@gmail.com>
2020-07-11 02:56:49 +0100

    Add files via upload

---
+++ PatternCount/R/PatternCount.R
@@ -0,0 +1,16 @@
+PatternCount <-function(Text, Pattern){
+  Text <- strsplit(Text, split=NULL)[[1]]
+  Pattern <- strsplit(Pattern, split=NULL)[[1]]
+  count<-0
+  if(length(Pattern)>length(Text)){
+    stop("Error: Length of pattern should be less than length of text")
+  }else{
+    stop("Error: Number of samples in training data expression matrix and phenodata do not match")
+    for(i in 1:(length(Text)-length(Pattern)+1)){
+      if(sum(Text[i:(i+length(Pattern)-1)] == Pattern) == length(Pattern)){
+        count <- count+1
+      }
+    }
+    return(count)
+  }
+}
