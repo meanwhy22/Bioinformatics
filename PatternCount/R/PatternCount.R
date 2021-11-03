a4b47bdf192fbff35ee28dfad9d4c9ddd2a6f81c
Minh Y Tran <90123614+meanwhy22@users.noreply.github.com>
2021-11-03 20:43:19 +0700

    Adapted from the original code
    Some cool stuffs I came across during a course on Coursera
    The orginal code is from @sanjaysinghrathi

--- PatternCount/R/PatternCount.R
+++ PatternCount/R/PatternCount.R
@@ -1,16 +1,14 @@
-PatternCount <-function(Text, Pattern){
+#This program counts the number of occurrences of a specific pattern in a given string 
+#For example, we want to count how many times a certain DNA pattern appears in a given DNA sequence (overlapping occurrences also included)
+
+PatternCount <- function(Text, Pattern){
   Text <- strsplit(Text, split=NULL)[[1]]
   Pattern <- strsplit(Pattern, split=NULL)[[1]]
-  count<-0
-  if(length(Pattern)>length(Text)){
-    stop("Error: Length of pattern should be less than length of text")
-  }else{
-    stop("Error: Number of samples in training data expression matrix and phenodata do not match")
-    for(i in 1:(length(Text)-length(Pattern)+1)){
-      if(sum(Text[i:(i+length(Pattern)-1)] == Pattern) == length(Pattern)){
-        count <- count+1
+  count <- 0
+  for(i in 1:(length(Text)-length(Pattern)+1)){
+    if(sum(Text[i:(i+length(Pattern)-1)] == Pattern) == length(Pattern)){
+       count <- count+1
       }
     }
-    return(count)
-  }
+  return(count)
 }
